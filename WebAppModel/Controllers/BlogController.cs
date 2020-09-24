402d3015a2cc27da33ba72668f01a3b98c32fde9
Breno Marques <breno.marques@hbsis.com.br>
2020-09-24 17:19:00 -0300

    Render dynamic content with Razor

---
+++ WebAppModel/Controllers/BlogController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebAppModel.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public BlogController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return new ContentResult { Content = "Blog posts" };;
+        }
+    }
+}
