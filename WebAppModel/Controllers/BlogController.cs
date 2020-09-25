36cee84b1b0383e1a3d69b87629e03481301778e
Breno Marques <breno.marques@hbsis.com.br>
2020-09-25 11:01:15 -0300

    Rendering reusable sections of content in a layout

--- WebAppModel/Controllers/BlogController.cs
+++ WebAppModel/Controllers/BlogController.cs
@@ -1,8 +1,10 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace WebAppModel.Controllers
 {
+    [Route("blog")]
     public class BlogController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -11,9 +13,20 @@ namespace WebAppModel.Controllers
         {
             _logger = logger;
         }
-
+        
+        [Route("")]
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [Route("{year:min(2000)}/{month:range(1,12)}/{key}")]
+        public IActionResult Post(int year, int month, string key)
+        {
+            ViewBag.Title = "My Blog post";
+            ViewBag.Posted = DateTime.Now;
+            ViewBag.Author = "Breno Marques";
+            ViewBag.Body = "My way";
             return View();
         }
     }
