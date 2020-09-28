d80deb63fa8409bae78d68a80020e7309a850af0
Breno Marques <breno.marques@hbsis.com.br>
2020-09-28 10:13:45 -0300

    Post form data to controller actions

--- WebAppModel/Controllers/BlogController.cs
+++ WebAppModel/Controllers/BlogController.cs
@@ -2,6 +2,7 @@ using System;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using WebAppModel.Models;
+using WebAppModel.Models.Dtos;
 
 namespace WebAppModel.Controllers
 {
@@ -24,18 +25,33 @@ namespace WebAppModel.Controllers
         [Route("{year:min(2000)}/{month:range(1,12)}/{key}")]
         public IActionResult Post(int year, int month, string key)
         {
-            var blogItem = new BlogItem(){
-                 Title = "My Blog post",
-                 Posted = DateTime.Now,
-                 Author = "Breno Marques",
-                 Body = "My way"
+            var blogItem = new BlogItem()
+            {
+                Title = "My Blog post",
+                Posted = DateTime.Now,
+                Author = "Breno Marques",
+                Body = "My way"
             };
             return View(blogItem);
         }
 
-        [Route("create")]
-        public IActionResult Create(){
+        [HttpGet, Route("create")]
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        [HttpPost, Route("create")]
+        public IActionResult Create(BlogItemDto blogItemDto)
+        {
+            var blogItem = new BlogItem
+            {
+                Title = blogItemDto.Title,
+                Body = blogItemDto.Body,
+                Author = User.Identity.Name,
+                Posted = DateTime.Now
+            };
+            return View(model: blogItem);
         }
     }
 }
---
+++ WebAppModel/Models/Dtos/BlogItemDto.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace WebAppModel.Models.Dtos
+{
+    public class BlogItemDto
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
